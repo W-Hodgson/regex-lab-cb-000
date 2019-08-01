d92aab6fb123b0c8eea55ec685d907bf8dc8d039
Will Hodgson <will.hodgson@current-rms.com>
2019-08-01 10:20:29 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.to_i.match(/\A\d{10}\z/) ? true : false
+  phone.match(/\A\d{10}\z/) ? true : false
 end
 
 valid_numbers = ["2438894546", "(718)891-1313", "234 435 9978", "(800)4261134"]
